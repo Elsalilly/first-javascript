7e514fed52a09ae0095001a187f677bc437cbd08
Elsalilly <elsa.mertala@gmail.com>
2025-03-19 13:03:40 +0100

    adding the three excerises

---
+++ main.js
@@ -0,0 +1,16 @@
+let message = "Hello, World!";
+console.log(`${message}`);
+
+let firstName = "Elsa";
+let age = 30;
+let favouriteColour = "green";
+
+console.log(`My names is ${firstName}, I am ${age} years old and my favourite colour is ${favouriteColour}.`);
+
+num1 = 7;
+num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
