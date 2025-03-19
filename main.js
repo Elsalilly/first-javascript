fafbea895832182b862cd9b6e1225871f0849c4f
Elsalilly <elsa.mertala@gmail.com>
2025-03-19 17:53:50 +0100

    added the second assignment, price and prompt

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ console.log(num1 + num2);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
+
+let answer = prompt("Please enter the price tag for bananas?");
+console.log(answer);
+
+let price = parseInt(answer);
+
+let percentage = 0.9;
+
+let totalValue =  (price * percentage);
+
+console.log(`Your new price after the discount is $${totalValue}.`);
+
+
+
+/*
+console.log(Number("answer"));
+*/
+
