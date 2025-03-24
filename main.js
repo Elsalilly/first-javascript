227c0f2612bcf835a51493a16a5398695537fff3
Elsalilly <elsa.mertala@gmail.com>
2025-03-24 12:07:26 +0100

    assignment 3 with isValidPassword/Username

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Assignment 1
 let message = "Hello, World!";
 console.log(`${message}`);
 
@@ -14,7 +15,10 @@ console.log(num1 + num2);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
+*/
 
+
+/* Assignment 2
 let answer = prompt("Please enter the price tag for bananas");
 
 console.log(answer);
@@ -28,10 +32,50 @@ let percentage = 0.9;
 let totalValue = (newPrice * percentage);
 
 console.log(`Your new price after the discount is $${totalValue.toFixed(2)}.`);
+*/
 
+
+/* Assignment 3 */
+
+
+function isValidUsername (username) {
+    if(!(username.length >= 8)) {
+        return false;
+    }
+    if (username.indexOf(" ") !== -1) {
+        return false;
+    }
+    return true;
+}
+
+function isValidPassword (password) {
+    if(!(password.length >= 8)) {
+        return false;
+    }
+    if (password.indexOf(" ") !== -1) {
+        return false;
+    }
+    if (password == "hellohello") {
+        return false;
+    }
+    return true;
+}
+
+console.log(isValidUsername("hellohello"));
+console.log(isValidPassword("hellohello"));
 
 
 /*
-console.log(Number("answer"));
-*/
+    if(!(password.length >= 8)) {
+        return false;
+    }
+}
 
+    return password.includes ("hellohello") ? true : false;
+const password1 = "helloeverybody";
+if (isValidPassword(password1)) {
+    console.log(`${password1} - Valid Password!`);
+} else {
+    console.log(`${password1} - Invalid Password!`);
+}
+*/
